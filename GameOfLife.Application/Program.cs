739cee65e0372c15ee92c2bb7823abf747da571f
Eugene Shamidanov <vectorrr.pro@gmail.com>
2019-09-30 11:45:53 +0300

    🎉 mvp

---
+++ GameOfLife.Application/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using GameOfLife.Model;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace GameOfLife.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uni = new Universe();
+            var window = new RenderWindow(new VideoMode(500, 500), "SFML running in .NET Core");
+            window.Closed += (_, __) => window.Close();
+            window.SetFramerateLimit(30);
+            
+            while (window.IsOpen)
+            {
+                window.DispatchEvents();
+                window.Clear(Color.White);
+                
+                foreach (var f in DrawField(uni))
+                {
+                    window.MouseButtonPressed += (sender, eventArgs) =>
+                    {
+                        if (eventArgs.Button == Mouse.Button.Left)
+                        {
+                            uni.SetFilled(eventArgs.X, eventArgs.Y);
+                        }
+                    };
+                    
+                    window.Draw(f);
+                }
+
+                window.Display();
+                uni.GameStep();
+            }
+        }
+                
+        
+        public static IEnumerable<RectangleShape> DrawField(Universe u)
+        {
+
+            for (int i = 0; i < 50; i++)
+            {
+                for (int j = 0; j < 50; j++)
+                {
+                    if (u.Field[i, j] == 0)
+                        yield return new RectangleShape(new Vector2f(10, 10))
+                        {
+                            FillColor = Color.White,
+                            OutlineColor = Color.Black,
+                            OutlineThickness = 1,
+                            Position = new Vector2f(j * 10, i * 10)
+                        };
+                    else
+                        yield return new RectangleShape(new Vector2f(10, 10))
+                        {
+                            FillColor = Color.Green,
+                            OutlineColor = Color.Black,
+                            OutlineThickness = 1,
+                            Position = new Vector2f(j * 10, i * 10)
+                        };
+                }
+            }
+
+        }
+    }
+}
---
+++ GameOfLife.Model/Universe.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Buffers;
+
+namespace GameOfLife.Model
+{
+    public class Universe
+    {
+        public const int UniverseSize = 500;
+        
+        public int[,] Field { get; }
+        public int[,] Field2 { get; }
+        
+        public Universe()
+        {
+            Field = new int[UniverseSize, UniverseSize];
+            Field2 = new int[UniverseSize, UniverseSize];
+
+            Field[6, 3] = 1;
+            Field[7, 4] = 1;
+            Field[8, 2] = 1;
+            Field[8, 3] = 1;
+            Field[8, 4] = 1;
+        }
+
+        public void Print()
+        {
+            for (var i = 0; i < Field.GetLength(0); i++)
+            {
+                for (var j = 0; j < Field.GetLength(1); j++)
+                {
+                    Console.Write(Field[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public void SetFilled(int x, int y)
+        {
+            var xx = (int)Math.Floor(Convert.ToDouble(x / 10));
+            var yy = (int)Math.Floor(Convert.ToDouble(y / 10));
+            
+            Field[yy, xx] = 1;
+        }
+
+        public int? WhatTodo(int x, int y)
+        {
+            var n = 0;
+            
+            for (var i = x - 1; i <= x + 1; i++)
+            {
+                for (var j = y - 1; j <= y + 1; j++)
+                {
+                    if (i == x && j == y)
+                        continue;
+
+                    if (i < 0 || j < 0)
+                        continue;
+                    if (i >= UniverseSize || j >= UniverseSize)
+                        continue;
+                    
+                    if (Field[i, j] == 1)
+                        n++;
+                }
+            }
+
+            if (n <= 1 || n >= 4)
+                return 0;
+
+            if (n == 3)
+                return 1;
+            
+            return null;
+        }
+
+        public void GameStep()
+        {
+            for (var i = 0; i < Field2.GetLength(0); i++)
+            {
+                for (var j = 0; j < Field2.GetLength(1); j++)
+                {
+                    Field2[i, j] = WhatTodo(i, j) ?? Field[i, j];
+                    
+                   // Console.Write(Field2[i, j]);
+                }
+
+                //Console.WriteLine();
+            }
+            
+            for (var i = 0; i < Field2.GetLength(0); i++)
+            {
+                for (var j = 0; j < Field2.GetLength(1); j++)
+                {
+                    Field[i, j] = Field2[i, j];
+                }
+            }
+            
+        }
+    }
+}
